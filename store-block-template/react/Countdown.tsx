442c3ef8407402b6c3c8d57607b072f2f77ce09e
vfcoelho <vfcoelho18.2@hotmail.com>
2020-10-17 02:19:50 -0300

    includes a prop and exposes it in site editor

--- store-block-template/react/Countdown.tsx
+++ store-block-template/react/Countdown.tsx
@@ -1,23 +1,32 @@
 import React from 'react'
 
 // Component definition of props
-interface CountdownProps { }
+interface CountdownProps {
+  targetDate: string
+}
 
 // Component
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate }) => {
   return (
     <div>
-      <h1>Countdown Test</h1>
+      <h1>{targetDate}</h1>
     </div>
   )
 }
 
-// Content show in site editor
+// Content shown in site editor
 Countdown.schema = {
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
-  properties: {},
+  properties: {
+    targetDate: {
+      title: 'Data final',
+      description: 'Data final utilizada no contador',
+      type: 'string',
+      default: null,
+    },
+  },
 }
 
 export default Countdown
