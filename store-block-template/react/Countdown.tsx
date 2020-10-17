630c28052d87fd273facfec67367152400b97d05
vfcoelho <vfcoelho18.2@hotmail.com>
2020-10-17 02:27:00 -0300

    adds countdown feature and changes block to use it

--- store-block-template/react/Countdown.tsx
+++ store-block-template/react/Countdown.tsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TimeSplit } from './typings/global'
+import { tick, getTwoDaysFromNow } from './utils/time'
 
 // Component definition of props
 interface CountdownProps {
   targetDate: string
 }
 
+const DEFAULT_TARGET_DATE = getTwoDaysFromNow()
+
 // Component
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate }) => {
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
+  targetDate = DEFAULT_TARGET_DATE
+}) => {
+
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00'
+  })
+
+  tick(targetDate, setTime)
+
   return (
     <div>
-      <h1>{targetDate}</h1>
+      <h1>{`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}</h1>
     </div>
   )
 }
